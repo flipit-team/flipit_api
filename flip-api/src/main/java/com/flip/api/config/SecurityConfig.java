5e3ea75de323cb598a650a266e582cea4f31b31c
Charles Okonkwo <ookonkwo@appzonegroup.com>
2021-11-26 13:34:01 +0100

    added authenticate method

--- flip-api/src/main/java/com/flip/api/config/SecurityConfig.java
+++ flip-api/src/main/java/com/flip/api/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.flip.api.config;
 import com.flip.service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
