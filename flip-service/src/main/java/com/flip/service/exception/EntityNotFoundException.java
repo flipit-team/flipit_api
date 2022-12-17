ec57452efb9a9ba9e3c0ce0d16d6c6c9cf6d2f58
Charles Okonkwo <ookonkwo@appzonegroup.com>
2022-12-17 01:04:40 -0500

    code cleanup

--- flip-service/src/main/java/com/flip/service/exception/EntityNotFoundException.java
+++ flip-service/src/main/java/com/flip/service/exception/EntityNotFoundException.java
@@ -13,15 +13,13 @@ public class EntityNotFoundException extends RuntimeException {
     }
 
     private static String generateMessage(String entity, Map<String, String> searchParams) {
-        return StringUtils.capitalize(entity) +
-                " was not found for parameters " +
-                searchParams;
+        return StringUtils.capitalize(entity) + " was not found for parameters " + searchParams;
     }
 
-    private static <K, V> Map<K, V> toMap(
-            Class<K> keyType, Class<V> valueType, Object... entries) {
+    private static <K, V> Map<K, V> toMap(Class<K> keyType, Class<V> valueType, Object... entries) {
         if (entries.length % 2 == 1)
             throw new IllegalArgumentException("Invalid entries");
+
         return IntStream.range(0, entries.length / 2).map(i -> i * 2)
                 .collect(HashMap::new,
                         (m, i) -> m.put(keyType.cast(entries[i]), valueType.cast(entries[i + 1])),
