182c6238c57d77c4b403b2083f584dfe9e048374
Charles Okonkwo <ookonkwo@appzonegroup.com>
2022-12-23 20:06:51 -0500

    updated AuthRequest

--- flip-service/src/main/java/com/flip/service/pojo/request/AuthRequest.java
+++ flip-service/src/main/java/com/flip/service/pojo/request/AuthRequest.java
@@ -6,19 +6,23 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 /**
  * @author Charles on 26/11/2021
  */
-@Component
 @Getter
 @Setter
+@Component
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthRequest implements Serializable {
 
+    @NotBlank(message = "Email is required")
     private String username;
+
+    @NotBlank(message = "Password is required")
     private String password;
 
 }
