7e07ec08c1d962cf1e74a1531082ff7c5a12d86a
Charles Okonkwo <ookonkwo@appzonegroup.com>
2023-02-02 02:01:20 -0500

    cleaned imports

--- flip-api/src/main/java/com/flip/FlipApiApplication.java
+++ flip-api/src/main/java/com/flip/FlipApiApplication.java
@@ -2,7 +2,6 @@ package com.flip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(scanBasePackages = {"com.flip"})
 public class FlipApiApplication {
